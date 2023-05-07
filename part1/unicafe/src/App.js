d7204a74bdc21c37674d80bd71b26759c71043fd
Tara Wade <tarawade1205@gmail.com>
2023-05-07 01:48:33 -0400

    Squashed commit of the following:
    commit 39ffde64a3c69cd7d042f91f32f5828590626a9c
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 01:41:45 2023 -0400
    
        Complete Exercise 1.14*
    
        Display most votes
    
    commit 00c79fcedcfd6970cdafa8f9f5fbe97daea3d7f3
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 01:29:23 2023 -0400
    
        Complete exercise 1.13*
    
        Add functionality to count votes
    
    commit dfefe63def7e4866ca24e5ef166c7f88ed9d730d
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 00:59:46 2023 -0400
    
        Complete Exercise 1.12*
    
    commit d3c15386a2d43980da967f0cd5b801f4e8aa7e82
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 00:44:56 2023 -0400
    
        Setup project
    
    commit 6acae14f24c42fd044666e8103dfb1b6c2360a23
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 00:36:13 2023 -0400
    
        Complete Exercise 1.12
    
    commit ff1ca1cde2d02b46e50e93d957e8169720281e29
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 00:30:36 2023 -0400
    
        Handle % using template strings
    
    commit c698072fc6764b972df4baa3844326e9e70b4973
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sun May 7 00:15:52 2023 -0400
    
        Modify to use variable for statistics
    
        Do not need to use states to calculate statistics. Can
        calculate statistics prior to setting value
    
    commit e9cd46ad45a1281a7c12448acd1595767e863af0
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sat May 6 23:56:45 2023 -0400
    
        Complete Exercise 1.10
    
    commit a2adde2601531d08e0ab4de6f98cfc12e25a2c9d
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sat May 6 23:45:17 2023 -0400
    
        Complete Exercise 1.9
    
    commit 47cba6645b0a7ea466b2489998071f86d7f5ba58
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sat May 6 23:41:35 2023 -0400
    
        Complete Exercise 1.8
    
    commit e185b6ce5b96203e472ead3c28d7a416f56a8e2f
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sat May 6 22:54:22 2023 -0400
    
        Complete Exercise 1.7
    
    commit 1918ceb72f84362315297925406f355b87e950e4
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Sat May 6 22:33:50 2023 -0400
    
        Complete Exercise 1.6
    
    commit 102f57e499c13079928998d03205c78110e9e13b
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Wed May 3 22:14:22 2023 -0400
    
        Complete Exercise 1.5
    
    commit 7bd3cfae214dd0f5418d49d6b1590c9f864796e1
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Wed May 3 22:06:20 2023 -0400
    
        Fix exercise 1.4
    
    commit 98a3d6bb5769ce71a9e66a05a9d5205f1ff8b3bf
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Wed May 3 21:44:55 2023 -0400
    
        Complete Exercise 1.4
    
    commit 44b58be5afd81b3eaa11086a30975c9684e91d18
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Wed May 3 21:38:55 2023 -0400
    
        Complete Exercise 1.3
    
    commit 4356f0e061cae2fb9770248a31882ad22ea96c03
    Author: Tara Wade <tarawade1205@gmail.com>
    Date:   Wed May 3 02:31:09 2023 -0400
    
        Finish exercise 1.1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+
+const Button = (props) =>
+{
+  return (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+}
+
+const StatisticsLine = (props) =>
+{
+  return (
+    <tr><td>{props.text}</td><td>{props.value}</td></tr>
+  )
+}
+
+const Statistics = (props) =>
+{
+  const totalFeedback = props.good + props.neutral + props.bad
+  const averageFeedback = (props.good - props.bad) / totalFeedback
+  const positiveFeedback = (props.good / totalFeedback) * 100
+  if (totalFeedback == 0)
+  {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  else 
+  {
+        return (
+      <table>
+        <tbody>
+          <StatisticsLine text='good' value={props.good} />
+          <StatisticsLine text='neutral' value={props.neutral} />
+          <StatisticsLine text='bad' value={props.bad} />
+          <StatisticsLine text='all' value={totalFeedback}/>
+          <StatisticsLine text='average' value={averageFeedback}/>
+          <StatisticsLine text='positive' value={`${positiveFeedback}%`} />
+        </tbody>
+      </table>
+    )
+  }
+
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+
+  const handleBad = () =>
+  {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handleNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
