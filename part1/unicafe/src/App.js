e185b6ce5b96203e472ead3c28d7a416f56a8e2f
Tara Wade <tarawade1205@gmail.com>
2023-05-06 22:54:22 -0400

    Complete Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,23 +15,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
-    // console.log('before good', good)
     const updatedGood = good + 1
-    // console.log('after good', updatedGood)
     setGood(updatedGood)
+    
+    const updatedAll = updatedGood + neutral + bad
+    setAll(updatedAll)
+    setAverage((updatedGood - bad) / updatedAll)
+    setPositive((updatedGood / updatedAll) * 100)
   }
 
   const handleNeutral = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+
+    const updatedAll = good + updatedNeutral + bad
+    setAll(updatedAll)
+    setAverage((good - bad) / updatedAll)
+    setPositive((good / updatedAll) * 100)
   }
 
   const handleBad = () =>
   {
     const updatedBad = bad + 1
     setBad(updatedBad)
+
+    const updatedAll = good + neutral + updatedBad
+    setAll(updatedAll)
+    setAverage((good - updatedBad) / updatedAll)
+    setPositive((good / updatedAll) * 100)
   }
 
   return (
@@ -44,6 +60,9 @@ const App = () => {
       <p> good {good} </p>
       <p> neutral {neutral} </p>
       <p> bad {bad} </p>
+      <p> all {all}</p>
+      <p> average {average}</p>
+      <p> positive {positive} % </p>
     </div>
   )
 }
